dc35b66ea3149afda63a173d1cf1de88a473cbdc
eitegbe <eitegbe@gmail.com>
2015-11-16 23:46:41 -0800

    first commit

---
+++ src/main/java/edu/sjsu/cmpe275Project/configs/SecurityConfig.java
@@ -0,0 +1,20 @@
+package edu.sjsu.cmpe275Project.configs;
+
+/*import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;*/
+
+/**
+ * Created by emy on 11/16/15.
+ */
+/*@Configuration
+@EnableWebSecurity*/
+public class SecurityConfig/* extends WebSecurityConfigurerAdapter*/ {
+
+  /*  @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        //super.configure(http);
+        http.authorizeRequests().antMatchers("/**").permitAll();
+    }*/
+}
