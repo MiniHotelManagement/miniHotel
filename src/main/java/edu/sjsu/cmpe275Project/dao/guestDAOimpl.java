70097e847657d793a05c1978375256a8c054f9e0
eitegbe <eitegbe@gmail.com>
2015-11-20 19:45:13 -0800

    modified dao

--- src/main/java/edu/sjsu/cmpe275Project/dao/guestDAOimpl.java
+++ src/main/java/edu/sjsu/cmpe275Project/dao/guestDAOimpl.java
@@ -2,11 +2,13 @@ package edu.sjsu.cmpe275Project.dao;
 
 import edu.sjsu.cmpe275Project.models.Guest;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 /**
  * Created by emy on 11/19/15.
  */
 @Repository("guestDAO")
+@Transactional
 public class guestDAOimpl implements guestDAO {
 
     @Override
