c2d17c4022fbcea4c9bca190281e73fa71f7e47a
Yingzhu Deng <deng_yz1016@hotmail.com>
2015-12-01 22:22:06 -0800

    add security

--- src/main/java/edu/sjsu/cmpe275Project/configs/SecurityConfig.java
+++ src/main/java/edu/sjsu/cmpe275Project/configs/SecurityConfig.java
@@ -1,11 +1,33 @@
 package edu.sjsu.cmpe275Project.configs;
 
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.servlet.configuration.EnableWebMvcSecurity;
 
 /**
  * Created by emy on 11/16/15.
  */
+@Configuration
+@ComponentScan
+@EnableWebMvcSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+        auth
+                .inMemoryAuthentication()
+                .withUser("admin").password("admin").roles("admin");
+    }
 
-public class SecurityConfig {
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable().authorizeRequests().antMatchers("/room/*").hasRole("admin").and().httpBasic();
 
+
+        //super.configure(http);
+    }
 }
