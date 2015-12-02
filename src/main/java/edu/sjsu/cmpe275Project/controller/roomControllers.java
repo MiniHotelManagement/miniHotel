519c2f2a17bba894640039f2fab93b2bb942cdf2
eitegbe <eitegbe@gmail.com>
2015-12-02 14:58:48 -0800

    commented out roomController and eventually will delete file

--- src/main/java/edu/sjsu/cmpe275Project/controller/roomControllers.java
+++ src/main/java/edu/sjsu/cmpe275Project/controller/roomControllers.java
@@ -20,47 +20,47 @@ import java.util.Collection;
 @Controller
 public class roomControllers {
 
-    @Autowired
-    private roomService roomservice;
-
-    @RequestMapping(value="/room", method = RequestMethod.POST)
-    @ResponseBody
-    public Room addRoom(@RequestBody Room room){
-        Room room1 = roomservice.create(room);
-        return room1;
-    }
-
-    @RequestMapping(value="/testroom")
-    //@ResponseBody
-    public void test(){
-        System.out.print("POST is allowed here");
-    }
-
-
-    @RequestMapping(value="/room/update/{id}", method = RequestMethod.POST)
-    @ResponseBody
-    public Room updateRoom(@Valid @PathVariable("id") long id, @RequestBody Room room){
-        Room room1 = roomservice.findById(id);
-        if(room1!=null) {
-            room.setId(id);
-            roomservice.update(id, room);
-        }
-        room1 = roomservice.findById(id);
-        return room1;
-    }
-
-    @RequestMapping(value="/room/{id}", method = RequestMethod.GET)
-    @ResponseBody
-    public Room getRoom(@Valid @PathVariable("id") long id){
-        Room room1 = roomservice.findById(id);
-        return room1;
-    }
-
-
-    @RequestMapping(value="/room/{id}", method = RequestMethod.DELETE)
-    @ResponseBody
-    public void deleteRoom(@Valid @PathVariable("id") long id){
-        roomservice.delete(id);
-    }
+//    @Autowired
+//    private roomService roomservice;
+//
+//    @RequestMapping(value="/room", method = RequestMethod.POST)
+//    @ResponseBody
+//    public Room addRoom(@RequestBody Room room){
+//        Room room1 = roomservice.create(room);
+//        return room1;
+//    }
+//
+//    @RequestMapping(value="/testroom")
+//    //@ResponseBody
+//    public void test(){
+//        System.out.print("POST is allowed here");
+//    }
+//
+//
+//    @RequestMapping(value="/room/update/{id}", method = RequestMethod.POST)
+//    @ResponseBody
+//    public Room updateRoom(@Valid @PathVariable("id") long id, @RequestBody Room room){
+//        Room room1 = roomservice.findById(id);
+//        if(room1!=null) {
+//            room.setId(id);
+//            roomservice.update(id, room);
+//        }
+//        room1 = roomservice.findById(id);
+//        return room1;
+//    }
+//
+//    @RequestMapping(value="/room/{id}", method = RequestMethod.GET)
+//    @ResponseBody
+//    public Room getRoom(@Valid @PathVariable("id") long id){
+//        Room room1 = roomservice.findById(id);
+//        return room1;
+//    }
+//
+//
+//    @RequestMapping(value="/room/{id}", method = RequestMethod.DELETE)
+//    @ResponseBody
+//    public void deleteRoom(@Valid @PathVariable("id") long id){
+//        roomservice.delete(id);
+//    }
 
 }
