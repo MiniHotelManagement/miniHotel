b9e49da35e58be4c1457f4b7ebe9d76529bb57f5
eitegbe <eitegbe@gmail.com>
2015-12-02 17:44:26 -0800

    fixed columndefinitionBug

--- src/main/java/edu/sjsu/cmpe275Project/models/Occupancy.java
+++ src/main/java/edu/sjsu/cmpe275Project/models/Occupancy.java
@@ -34,7 +34,7 @@ public class Occupancy implements Serializable {
 
     private int NunOfPerson;
 
-    @Column(name="status", columnDefinition = "default 0")
+//    @Column(name="status", columnDefinition = "default 0")
     private int status; // 0:reserved 1:cheched-in 2:checked-out
 
     private Date checkInDate;
